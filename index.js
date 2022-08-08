6d1fbf1cee0f121b52123e7ff2f89cde1c6906ad
haolegumi <hao.le@gumiviet.com>
2022-08-08 09:33:18 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 const app = express()
+
 dotenv.config()
 
-app.get('/ping', (_, res) => {
+app.get('/', (_, res) => {
   return res.send({ message: 'Hello, world!' })
 })
 
