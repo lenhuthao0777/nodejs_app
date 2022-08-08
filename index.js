f5ab5dbfe7bf3401e6ae245d7c044beffeb6d534
haolegumi <hao.le@gumiviet.com>
2022-08-08 09:29:21 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
 
-app.listen(3000)
+app.get('/ping', (_, res) => {
+  return res.send({ message: 'Hello, world!' })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  return console.log('abc')
+})
